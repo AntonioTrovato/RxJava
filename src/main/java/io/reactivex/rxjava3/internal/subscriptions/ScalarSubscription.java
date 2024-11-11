9822e720f0937f4d6a70ed28a2d045cd77c898f7
Antonio Trovato <atrovato@unisa.it>
2024-11-11 22:01:01 +0100

    trying gradlew jmhJar

--- src/main/java/io/reactivex/rxjava3/internal/subscriptions/ScalarSubscription.java
+++ src/main/java/io/reactivex/rxjava3/internal/subscriptions/ScalarSubscription.java
@@ -62,7 +62,6 @@ public final class ScalarSubscription<T> extends AtomicInteger implements QueueS
 
     @Override
     public void cancel() {
-        System.out.println("hey");
         lazySet(CANCELLED);
     }
 
